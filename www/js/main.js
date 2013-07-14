6fedd654e04360cd331a318d4d163684d4af8ee2
Andrew Morrison <amorrison@etsy.com>
2013-07-14 18:47:41 +0000

    divorcing jquery and require

--- www/js/main.js
+++ www/js/main.js
@@ -9,7 +9,8 @@ require.config({
   },
   paths: {
     Easel: 'vendor/easeljs-0.6.1.min',
-    Underscore: 'vendor/underscore-min'
+    Underscore: 'vendor/underscore-min',
+    jquery: 'vendor/jquery-1.10.2.min'
   }
 });
 
