9f89f839f16ed9d48b7ebf63a116ec423cf85763
Andrew Morrison <amorrison@etsy.com>
2016-06-26 15:15:37 +0000

    Adding desktop app link back

--- templates/footer.php
+++ templates/footer.php
@@ -3,8 +3,8 @@
     <ul>
       <li><a id="link-home" data-name="home" href="/"><?= $host_name ?></a></li>
       <li> by <a id="link-author" data-name="author" href="http://twitter.com/asmorrison" target="_blank">@asmorrison</a>.</li>
-      <?php if(false && $is_mac): ?>
-        <li><a id="link-app" data-name="app" href="https://itunes.apple.com/us/app/snapnote/id687080181">Install the Desktop App.</a></li>
+      <?php if($is_mac): ?>
+        <li><a id="link-app" data-name="app" href="http://snapnote.io/app/Snapnote.dmg">Install the Desktop App.</a></li>
       <?php endif ?>
     </ul>
   </div>
